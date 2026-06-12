40f55d31d20c54b99a595776b1b585bb880ffe9d
Chinz266 <chinkung38@gmail.com>
2026-06-12 20:28:52 +0700

    add CRUD admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminController } from './controller/admin.controller';
+import { AdminService } from './service/admin.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminEntity } from './entity/admin.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',      // ใส่ username ของ MySQL
+      password: '',  // ใส่ password ของ MySQL
+      database: 'water-bill-db',     // ใส่ชื่อฐานข้อมูลที่คุณสร้างไว้
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,     // แนะนำให้เปิด true แค่ตอน Dev (มันจะสร้างตารางให้ตาม Entity อัตโนมัติ)
+    }),
+    TypeOrmModule.forFeature([AdminEntity])
+  ],
+  controllers: [AppController, AdminController],
+  providers: [AppService, AdminService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,19 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. ตั้งค่าพื้นฐานสำหรับหน้า Document
+  // เริ่มต้นตั้งค่า Swagger
   const config = new DocumentBuilder()
-    .setTitle('Smart Water Bill API')
-    .setDescription('API documentation for the water bill service')
-    .setVersion('1.0')
+    .setTitle('My API Documentation') // ชื่อ API ของคุณ
+    .setDescription('The API description') // คำอธิบาย
+    .setVersion('1.0') // เวอร์ชัน
+    .addBearerAuth() // เพิ่มระบบ Authentication (ถ้ามี)
     .build();
-
-  // 2. สร้าง Document จาก config
+    
   const document = SwaggerModule.createDocument(app, config);
   
-  // 3. ผูก Swagger UI ไว้ที่ path '/api-docs'
+  // กำหนด path สำหรับเข้าดู Swagger UI (ในตัวอย่างนี้คือ /api)
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
